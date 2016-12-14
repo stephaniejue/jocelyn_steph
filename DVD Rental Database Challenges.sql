64fd35991c3e408d7748756e3b0d2554d836f5b9
steph, jocelyn <stephandjocelyn@LEARNacademy.com>
2016-12-14 11:07:28 -0800

    Updated with month selector

--- DVD Rental Database Challenges.sql
+++ DVD Rental Database Challenges.sql
@@ -52,25 +52,47 @@ FROM
 WHERE
 	films.title = 'Shock Cabin'
 
-  -- JuneSELECT DISTINCT
-	pays.payment_id,
-	pays.payment_date,
-	custs.first_name,
-	custs.last_name,
-	adresses.district
-FROM
-	payments AS pays JOIN
-	customers AS custs ON (pays.customer_id = custs.customer_id) JOIN
-	rentals AS rents ON (custs.customer_id = rents.customer_id) JOIN
-	inventory AS inv ON (rents.inventory_id = inv.inventory_id) JOIN
-	films ON (inv.film_id = films.film_id),
-	adresses
-WHERE
-	custs.address_id = adresses.address_id AND
-	adresses.district LIKE '%Texas%' AND (
-	MONTH( `TimeStamp` ) =06)
-ORDER BY
-	pays.payment_id
+  -- JuneSELECT DISTINCT (by payments)
+  SELECT DISTINCT
+  	pays.payment_id,
+  	pays.payment_date,
+  	custs.first_name,
+  	custs.last_name,
+  	adresses.district
+  FROM
+  	payments AS pays JOIN
+  	customers AS custs ON (pays.customer_id = custs.customer_id) JOIN
+  	rentals AS rents ON (custs.customer_id = rents.customer_id) JOIN
+  	inventory AS inv ON (rents.inventory_id = inv.inventory_id) JOIN
+  	films ON (inv.film_id = films.film_id),
+  	adresses
+  WHERE
+  	custs.address_id = adresses.address_id AND
+  	adresses.district LIKE '%Texas%' AND
+  	EXTRACT (MONTH FROM pays.payment_date) > 1 --There were no payments in June
+  ORDER BY
+  	pays.payment_id
+
+    --June Select (by rentals); to make distinct, removed payments
+  SELECT
+  	rents.rental_id,
+  	rents.rental_date,
+  	films.title,
+  	custs.first_name,
+  	custs.last_name,
+  	adresses.district
+  FROM
+  	customers AS custs JOIN
+  	rentals AS rents ON (custs.customer_id = rents.customer_id) JOIN
+  	inventory AS inv ON (rents.inventory_id = inv.inventory_id) JOIN
+  	films ON (inv.film_id = films.film_id),
+  	adresses
+  WHERE
+  	custs.address_id = adresses.address_id AND
+  	adresses.district LIKE '%Texas%' AND
+  	EXTRACT (MONTH FROM rents.rental_date) = 6
+  ORDER BY
+  	rents.rental_id
 
 -- How many orders have we had for sci-fi films? From Texas?
 
