88aaf7374ca2462c64017ee9bbdf9caac3770aeb
Stephanie Chin <stephjuechin@gmail.com>
2016-12-30 16:25:17 -0800

    Moved address book app to new folder

--- address_book_app/db/migrate/20161221063112_add_contact_to_phones.rb
+++
@@ -1,5 +0,0 @@
-class AddContactToPhones < ActiveRecord::Migration
-  def change
-    add_reference :phones, :contact, index: true, foreign_key: {on_delete: :restrict, on_update: :restrict}
-  end
-end
